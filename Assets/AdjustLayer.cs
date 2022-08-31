a5d876b9614d3998279ff349ccec30bd2dbbd5dd
Luka Rizzi <lukafranco2000@hotmail.com>
2022-08-30 21:14:08 -0300

    adjust layer

---
+++ Assets/AdjustLayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustLayer : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        float _targetY = target.position.y;
+        bool _behindTarget = _targetY > transform.position.y;
+
+        if (_behindTarget)
+        {
+            sr.sortingOrder = 2;
+        }
+        else
+        {
+            sr.sortingOrder = 0;
+        }
+    }
+}

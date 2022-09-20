2f531d7f485568217f90ae8e80677c3c6d201154
Alexis Leandro Vallejos <49035235+AlexisLeandroVallejos@users.noreply.github.com>
2022-09-20 20:49:56 -0300

    spider prefab y anim

---
+++ Assets/Scripts/EnemyStates/SpiderStateWalking.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace RPGUNDAV.Gameplay
+{
+    public class SpiderStateWalking : EnemyState
+    {
+        public override void StartState(EnemyStateManager manager)
+        {
+
+        }
+        
+        public override void UpdateState(EnemyStateManager manager)
+        {
+            manager.rb.velocity = manager.transform.up * manager.speed;
+
+            RaycastHit2D hitWall = Physics2D.Raycast(manager.transform.position, manager.transform.up, manager.raycastWallDistance, manager.whatIsSolid);
+            
+            if (hitWall)
+            {
+                manager.transform.rotation = Quaternion.Euler(
+                    manager.transform.rotation.eulerAngles.x,
+                    manager.transform.rotation.eulerAngles.y,
+                    manager.transform.rotation.eulerAngles.z + 90);
+            }
+
+            Debug.DrawRay(manager.transform.position, manager.transform.up * manager.raycastWallDistance, Color.green, .01f);
+        }
+
+        public override void OnCollisionWithPlayer(EnemyStateManager manager, GameObject player)
+        {
+            player.GetComponent<PlayerStateManager>().Attacked(manager.gameObject);
+        }
+    }
+}

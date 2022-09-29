0f621350d5d160b1a14d710e14cb6acb9a3f1e60
ioelion <51096656+ioelion@users.noreply.github.com>
2022-09-29 17:08:25 -0300

    BombUsable funcionando

--- Assets/Scripts/ExplosiveBehaviour.cs
+++ Assets/Scripts/ExplosiveBehaviour.cs
@@ -7,31 +7,30 @@ namespace RPGUNDAV.Gameplay{
     {
         public float timeTillExplosion;
         public float explosionRadius;
-        public int damageToEnemies;
-        public int damageToPlayer;
         private CircleCollider2D bombCollider;
         private float timeAtActivation;
         // Logica incompleta terminar explosion
         void Start()
         {
             bombCollider = GetComponentInParent<CircleCollider2D>();
-            timeAtActivation = Time.deltaTime;
-            
+            timeAtActivation = Time.fixedTime;
+            bombCollider.enabled = false;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(Time.deltaTime - timeAtActivation >= timeTillExplosion){
+            if(Time.fixedTime >= timeAtActivation + timeTillExplosion){
+                bombCollider.enabled = true;
                 bombCollider.radius = explosionRadius;
-                bombCollider.isTrigger = true;
             }
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if(other.CompareTag("PlayerHurtBox")){
-                other.GetComponent<PlayerStateManager>().Attacked(this.gameObject);
+                GameObject playerHurtBox = other.gameObject;
+                playerHurtBox.GetComponentInParent<PlayerStateManager>().Attacked(this.gameObject);
                 Destroy(this.gameObject);
             }
         }

898c4e6d44fb1690f562bf1c509fa58f653e765e
ioelion <51096656+ioelion@users.noreply.github.com>
2022-09-29 11:58:18 -0300

    ExplosiveBehaviour no colisiona con jugador

--- Assets/Scripts/ExplosiveBehaviour.cs
+++ Assets/Scripts/ExplosiveBehaviour.cs
@@ -2,18 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExplosiveBehaviour : MonoBehaviour
-{
-    // Logica incompleta terminar explosion
-    // Start is called before the first frame update
-    void Start()
+namespace RPGUNDAV.Gameplay{
+    public class ExplosiveBehaviour : MonoBehaviour
     {
-        
-    }
+        public float timeTillExplosion;
+        public float explosionRadius;
+        public int damageToEnemies;
+        public int damageToPlayer;
+        private CircleCollider2D bombCollider;
+        private float timeAtActivation;
+        // Logica incompleta terminar explosion
+        void Start()
+        {
+            bombCollider = GetComponentInParent<CircleCollider2D>();
+            timeAtActivation = Time.deltaTime;
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+            if(Time.deltaTime - timeAtActivation >= timeTillExplosion){
+                bombCollider.radius = explosionRadius;
+                bombCollider.isTrigger = true;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.CompareTag("PlayerHurtBox")){
+                other.GetComponent<PlayerStateManager>().Attacked(this.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
     }
 }

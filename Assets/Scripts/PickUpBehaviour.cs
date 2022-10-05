019496be734a7a5d1ab47b0539d7b0735c029686
Luka Rizzi <lukafranco2000@hotmail.com>
2022-10-04 22:16:38 -0300

    coins have particle system

--- Assets/Scripts/PickUpBehaviour.cs
+++ Assets/Scripts/PickUpBehaviour.cs
@@ -8,6 +8,8 @@ namespace RPGUNDAV.Gameplay
         public int value = 1;
         [SerializeField] private LevelManager lm;
 
+        [SerializeField] private ParticleSystem particleSystem;
+
         public PickUp pickUp;
         
         private void Awake()
@@ -19,14 +21,22 @@ namespace RPGUNDAV.Gameplay
             if (collision.CompareTag("PlayerHurtBox"))
             {
                 lm.AddPickUpToPlayer(pickUp,value);
+
+                if (particleSystem != null)
+                {
+                    particleSystem.transform.parent = null;
+                    particleSystem.Emit(30);
+                    Destroy(particleSystem.gameObject, 2f);
+                }
+
+                OneTimePickup OTPickup = GetComponent<OneTimePickup>();
+
+                if (OTPickup != null)
+                {
+                    OTPickup.PickedUp();
+                }
+
                 Destroy(this.gameObject);
-            }
-
-            OneTimePickup OTPickup = GetComponent<OneTimePickup>();
-
-            if (OTPickup != null)
-            {
-                OTPickup.PickedUp();
             }
         }
     }
